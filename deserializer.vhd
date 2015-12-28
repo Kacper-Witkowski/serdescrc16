1dc780f0f6f7aec81d0c4b7b107b38a794b10868
kacaperek <kacaperek@gmail.com>
2015-12-28 21:22:46 +0100

    Z SERDESa. Nie ruszane.

--- deserializer.vhd
+++ deserializer.vhd
@@ -1,15 +1,13 @@
+----------------------------------------------------------------------------------
+-- Company: PWr
+-- Engineer: Kacper Witkowski
+	
+-- Module Name:    Serializer
+-- Project Name: 	 Nadajnik i odbiornik szeregowy z kontrolą poprawności przesyłu CRC16
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity deserializer is
     Port ( reset : in  STD_LOGIC;
@@ -20,7 +18,7 @@ end deserializer;
 
 architecture Behavioral of deserializer is
 
-signal c : std_logic_vector(2 downto 0);
+signal cnt : std_logic_vector(2 downto 0);
 signal d : std_logic_vector(7 downto 0);
 
 begin
@@ -28,27 +26,24 @@ begin
 counter : process (reset, clk)
 begin
     if reset = '0' then
-        c <= (others => '1');
+        cnt <= (others => '1');
     elsif (clk'event and clk = '1') then
-        if c = "111" then
-            c <= "000";
-        else
-            c <= c + "01";
-        end if;
+        cnt <= cnt + "01";
     end if;        
 end process counter;
 
-deser_p : process (reset, clk)
+sipo : process (reset, clk)
 begin
     if reset = '0' then
         d <= (others => '0');
     elsif (clk'event and clk = '1') then
-        if c ="000" then
+        if cnt ="001" then
             parallel_out <= d;
             d(7 downto 0) <= d(6 downto 0) & serial_in;
         else
             d(7 downto 0) <= d(6 downto 0) & serial_in;            
         end if;
     end if;    
-end process deser_p;
+end process sipo;
+
 end Behavioral;
